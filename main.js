ba5b9874fcee9c6f32a0661d8af596d6d1e5e0a5
krischanthas <kris.chanthasiriphan@gmail.com>
2018-07-24 10:46:47 -0700

    started modal and carousel

--- main.js
+++ main.js
@@ -22,10 +22,11 @@ function clearInput(){
     $('.inputForm').val('');
 }
 
-function myMap() {
+function displayMap() {
     var mapProp= {
         center:new google.maps.LatLng(51.508742,-0.120850),
         zoom:5,
     };
     var map=new google.maps.Map(document.getElementById("googleMap"),mapProp);
-    }
+}
+
