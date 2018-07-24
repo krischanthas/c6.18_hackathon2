daa6c5ac9fb0fc0d3039541125c440d26550736f
Tien Pham <tmpham1@uci.edu>
2018-07-24 11:37:53 -0700

    fixed getData function

--- main.js
+++ main.js
@@ -32,26 +32,28 @@ function myMap() {
     }
 
     function getData() {
-        var the_data = {
-            term: "beaches",
-            location: "irvine",
+    var settings = {
+        "url": "https://yelp.ongandy.com/businesses",
+        "method": "POST",
+        "dataType": "JSON",
+        "data": {
+            term: "beach",
+            location: "irvine", 
+            api_key: "w5ThXNvXEMnLlZYTNrvrh7Mf0ZGQNFhcP6K-LPzktl8NBZcE1_DC7X4f6ZXWb62mV8HsZkDX2Zc4p86LtU0Is9kI0Y0Ug0GvwC7FvumSylmNLfLpeikscQZw41pXW3Yx",
+        },
+        success: function(response) {
+            console.log(response);
+        },
+        error: function(err) {
+            console.log(err);
         }
-        var ajaxOption =  {
-            data: the_data,
-            dataType: 'json',
-            url: "https://api.yelp.com/v3/businesses/search",
-            headers: {
-                Authorization: "Bearer w5ThXNvXEMnLlZYTNrvrh7Mf0ZGQNFhcP6K-LPzktl8NBZcE1_DC7X4f6ZXWb62mV8HsZkDX2Zc4p86LtU0Is9kI0Y0Ug0GvwC7FvumSylmNLfLpeikscQZw41pXW3Yx",
-                "Cache-Control": "no-cache",
-                "Postman-Token": "9ed2ef7e-20f4-4f46-9901-3f48cb726c22",
-              },
-            method: 'GET',
-            success: function(response) { 
-               console.log(response);
-                    }
     }
-    $.ajax(ajaxOption);
-    }
+    
+    $.ajax(settings);
+    
+}
+
+    
 //     function getPhotos() {
 //         var the_data = {
 //             api_key: "b5e905e415b7b888752b23f5629b2410",
