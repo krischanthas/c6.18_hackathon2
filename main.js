7717212b2ce43279a8cd2c99f2f9230ec2eb7fc9
leahchoi <soora913@gmail.com>
2018-07-24 21:19:06 -0700

    added video modal and removes on click button

--- main.js
+++ main.js
@@ -4,18 +4,22 @@ var userObj = {};
 
 
 function initializeApp() {
-    applyClickHandlers();
 
+    applyClickHandlers();
+    getDataPhotos();
+    inputEnter();
 }
 
 function applyClickHandlers() {
     console.log('in applyClickHandlers')
     $('.submitButton').on('click', getUserInput);
+    $('.submitButton, .clearButton').on('click', removeVideo);
     $('.clearButton').on('click', clearInput);
-    $('.submitButton').on('click', )
-    $('.')
-}
 
+}
+function removeVideo(){
+    $('.iframe').addClass('hidden')
+}
 function getUserInput() {
     userInput = $('.inputForm').val();
     console.log(userInput);
@@ -23,7 +27,6 @@ function getUserInput() {
     //getVideoData();
     getData(userInput);
 
-    getVideoData();
     getDataPhotos();
     displayModal();
 }
@@ -32,7 +35,19 @@ function clearInput() {
     $('.inputForm').val('');
 }
 
+function inputEnter() {
+    $('input').keydown(function (e) {
+        if (e.keyCode == 13) {
+            $('.submitButton').click();
+        }
+    });
+}
+
+
+
+
 function displayMap() {
+
     var lati = 33.634867;
     var long = -117.740499;
 
@@ -60,35 +75,29 @@ function displayMap() {
         },
     });
     google.maps.event.addListener(marker, 'click', getVideoData);
-
-    // $(".container").append(map);
+    marker.addListener('click', toggleBounce);
+    $(".container").append(map);
 }
 
-
-
-// function toggleBounce() {
-//     if (marker.getAnimation() !== null) {
-//         marker.setAnimation(null);
-//     } else {
-//         marker.setAnimation(google.maps.Animation.BOUNCE);
-//     }
-// }
+function toggleBounce() {
+    if (marker.getAnimation() !== null) {
+        marker.setAnimation(null);
+    } else {
+        marker.setAnimation(google.maps.Animation.BOUNCE);
+    }
+}
 
 function checkNames(response) {
     for (var i = 0; i < response.businesses.length; i++) {
         var indivName = response.businesses[i].name;
+        console.log(indivName);
         if (indivName === userInput) {
             userObj = response.businesses[i];
             break;
         }
-
     }
     displayMap();
 }
-//     getDataPhotos();
-
-
-
 
 function displayModal() {
     var name = userObj.name;
@@ -100,13 +109,9 @@ function displayModal() {
     //display the name, url, & the pictures onto the modal
 }
 
-
-
-
-
-
 //yelp data
 function getData(userInput) {
+    debugger;
     var settings = {
         "url": "https://yelp.ongandy.com/businesses",
         "method": "POST",
@@ -118,19 +123,22 @@ function getData(userInput) {
             categories: "beaches",
         },
         success: function (response) {
-            // debugger;
-            console.log('getData success: ', response);
+
             checkNames(response);
         },
         error: function (err) {
-            // debugger;
+
             console.log('getData error: ', err);
         }
     }
 
     $.ajax(settings);
 
-    //checkNames();
+
+
+
+
+    displayPictures();
     //display the name, url, & the pictures onto the modal
 }
 
@@ -170,16 +178,18 @@ function getVideoData() {
         method: 'POST',
         url: 'https://s-apis.learningfuze.com/hackathon/youtube/search.php',
         success: function (response) {
+            displayVideo(response);
+            console.log('success response', response);
             var videoData = response["video"][0].id;
             console.log('www.youtube.com/watch?v=' + videoData);
-            $('.modal-body.video-body').append('<iframe>', {
+            $('.modal-body .video-body').append('<iframe>', {
                 attr: {
-                    src: 'https://www.youtube.com/watch?v='+videoData,
+                    src: 'https://www.youtube.com/watch?v=' + videoData,
                     class: 'videoPopUp'
 
                 }
             })
-            
+
         },
         error: function (response) {
             console.log('request error');
@@ -188,52 +198,54 @@ function getVideoData() {
     $.ajax(ajaxConfig);
 }
 
+function displayVideo(response) {
+    console.log('displayVideo success response', response);
+    var videoData = response["video"][0].id;
+    console.log('www.youtube.com/watch?v=' + videoData);
+    $('.iframe').removeClass('hidden')
+    $('.iframe').attr("src", 'https://www.youtube.com/embed/' + videoData).addClass("videoPopUp")
+    // $('.modal-body .video-body').append('<iframe>', {
+    //     attr: {
+    //         src: 'https://www.youtube.com/watch?v='+videoData,
+    //         class: 'videoPopUp'
 
-function getWeatherData(userInput){
+    //     }
+    // })
+}
+
+
+function getWeatherData(userInput) {
     var ajaxConfig = {
         url: 'http://api.openweathermap.org/data/2.5/weather?q=' + userInput + '&units=imperial&APPID=f91cd80c3f28fab67ca696381fb71d30',
         dataType: 'json',
         method: 'get',
-        success: function(response) {
+        success: function (response) {
             var weather = response.main.temp;
             console.log(weather);
             var cityName = response.name;
-           $('.mainDisplay').text(`Current weather: ${weather}`); 
-           $('.modal-title').text(cityName);    
+            $('.mainDisplay').text(`Current weather: ${weather}`);
+            $('.modal-title').text(cityName);
         },
-        error: function (){
+        error: function () {
             console.log('requestError');
         }
     }
-    
+
     $.ajax(ajaxConfig);
 }
 
+
 getPlaceID();
+
 function getPlaceID() {
     var theData = {
         api_key: "b5e905e415b7b888752b23f5629b2410",
         method: "flickr.places.findByLatLon",
         format: "json",
         nojsoncallback: 1,
-        lat:33.6189,
-        lon:  -117.9298,
+        lat: 33.6189,
+        lon: -117.9298,
         accuracy: 11,
-    }
-
-//     $.ajax(ajaxConfig);
- }
-
-
-function getDataPhotos() {
-    var theData = {
-        api_key: "b5e905e415b7b888752b23f5629b2410",
-        method: "flickr.photos.search",
-        format: "json",
-        nojsoncallback: 1,
-        text: "newport beach",
-        privacy_filter: 1,
-        per_page: 3,
     }
 
     var ajaxOption = {
@@ -243,19 +255,44 @@ function getDataPhotos() {
         method: 'GET',
         success: function (response) {
             console.log(response);
+        }
+
+    }
+    $.ajax(ajaxOption);
+
+}
+
+
+function getDataPhotos() {
+    var theData = {
+        api_key: "b5e905e415b7b888752b23f5629b2410",
+        method: "flickr.photos.search",
+        format: "json",
+        nojsoncallback: 1,
+        text: userInput,
+        privacy_filter: 1,
+        per_page: 3,
+        tags: "beach, sunset",
+    }
+
+    var ajaxOption = {
+        data: theData,
+        dataType: 'json',
+        url: "https://api.flickr.com/services/rest",
+        method: 'GET',
+        success: function (response) {
+            debugger;
+            console.log(response);
             var photoArray = response.photos.photo;
             console.log(photoArray);
             clearCarousel();
             for (var pIndex = 0; pIndex < photoArray.length; pIndex++) {
-
                 var currentPhoto = photoArray[pIndex];
                 var serverID = currentPhoto.server;
                 var photoID = currentPhoto.id;
                 var secretID = currentPhoto.secret;
                 var url = "https://farm1.staticflickr.com/" + serverID + "/" + photoID + "_" + secretID + ".jpg";
-                console.log(url);
                 var carouselImage = $(".carousel-image" + (pIndex + 1));
-                console.log(carouselImage)
                 carouselImage.prepend('<img src="' + url + '" />');
                 carouselImage.children().addClass("d-block w-100");
             }
@@ -265,12 +302,10 @@ function getDataPhotos() {
     $.ajax(ajaxOption);
 }
 
-function clearCarousel(){
+function clearCarousel() {
     $('.carousel-item').empty();
 }
 
-function clearCarousel(){
+function clearCarousel() {
     $('.carousel-item').empty();
 }
-
-
